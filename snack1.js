7fd0fc6db568e4587d0669852b72d18c4a22ec83
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-04-24 10:12:42 +0200

    code question 1

---
+++ snack1.js
@@ -0,0 +1,7 @@
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name); //1, Double chees burger
+console.log(secondBurger.name); //1, Double chees burger
